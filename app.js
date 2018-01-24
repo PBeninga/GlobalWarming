355644951e261247bca27715df2f0ae9c47dffb0
Patrick Beninga <patrickbeninga@gmail.com>
2018-01-24 14:42:56 -0800

    changing around server layout

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var app = express();
-
+const routes = require('./routes');
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-   response.send('Hello World!')
-});
-
+app.use('/', routes);
 app.listen(app.get('port'), function() {
      console.log("Node app is running at localhost:" + app.get('port'))
 });
