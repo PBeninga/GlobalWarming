c86915e7ed7aa3de7cf0db0b366d2b47120892e7
Patrick Beninga <patrickbeninga@gmail.com>
2018-02-20 13:13:09 -0800

    changed around client/server communication sockets

--- app.js
+++ app.js
@@ -12,19 +12,11 @@ app.use('/client',express.static(__dirname + '/client'));
 
 serv.listen(process.env.PORT || 2000);
 console.log("Server started.");
-var player_ids = new Map();
 var player_list = [];
-
 
 function onInputFired(){}
 // when a new player connects, we make a new instance of the player object,
 // and send a new player message to the client.
-function uuid() {
-   var S4 = function() {
-      return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-   };
-   return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
-}
 //call when a client disconnects and tell the clients except sender to remove the disconnected player
 function onClientdisconnect() {
 	console.log('disconnect');
@@ -54,24 +46,18 @@ function find_playerid(id) {
 
 	return false;
 }
-
-function newPlayer(){
+function onNewClient(){
+   this.broadcast.emit('newPlayer',{id:this.id});
+   this.emit('connected',{id:this.id, players:player_list});
+	player_list.push(this.id);
 
 }
- // io connection
+// io connection
 var io = require('socket.io')(serv,{});
 
 io.sockets.on('connection', function(socket){
-	console.log("socket connected")
-   let pubPlayerId = uuid();
-   let privatePlayerId = uuid();
-
-	player_list.push(pubPlayerId);
-   player_ids.set(privatePlayerId, pubPlayerId);
-   socket.on("new_player", newPlayer);
-   socket.broadcast.emit('new_player', {id: pubPlayerId});
-
-   socket.emit("id",{privateId: privatePlayerId, publicId: pubPlayerId});
+	console.log("socket connected");
+   socket.on("client_started", onNewClient);
 	// listen for disconnection;
 	socket.on('disconnect', onClientdisconnect);
 	//listen for new player inputs.
--- client/main.js
+++ client/main.js
@@ -14,9 +14,10 @@ var gameProperties = {
 var main = function(game){
 };
 
-function onsocketConnected () {
+function onsocketConnected (data) {
 	console.log("connected to server");
-	createPlayer();
+   console.log(this.id+" "+data.id)
+   enemies = data.players.slice();
 	gameProperties.in_game = true;
 	// send the server our initial position and tell it we are connected
 }
@@ -30,156 +31,39 @@ function onRemovePlayer (data) {
 		console.log('Player not found: ', data.id)
 		return;
 	}
-
-	removePlayer.player.destroy();
 	enemies.splice(enemies.indexOf(removePlayer), 1);
-}
-
-function createPlayer () {
-	player = game.add.graphics(0, 0);
-	player.radius = 100;
-
-	// set a fill and line style
-	player.beginFill(0xffd900);
-	player.lineStyle(2, 0xffd900, 1);
-	player.drawCircle(0, 0, player.radius * 2);
-	player.endFill();
-	player.anchor.setTo(0.5,0.5);
-	player.body_size = player.radius;
-
-	// draw a shape
-	game.physics.p2.enableBody(player, true);
-	player.body.clearShapes();
-	player.body.addCircle(player.body_size, 0 , 0);
-	player.body.data.shapes[0].sensor = true;
-}
-
-// this is the enemy class.
-var remote_player = function (id, startx, starty, start_angle) {
-	this.x = startx;
-	this.y = starty;
-	//this is the unique socket id. We use it as a unique name for enemy
-	this.id = id;
-	this.angle = start_angle;
-
-	this.player = game.add.graphics(this.x , this.y);
-	this.player.radius = 100;
-
-	// set a fill and line style
-	this.player.beginFill(0xffd900);
-	this.player.lineStyle(2, 0xffd900, 1);
-	this.player.drawCircle(0, 0, this.player.radius * 2);
-	this.player.endFill();
-	this.player.anchor.setTo(0.5,0.5);
-	this.player.body_size = this.player.radius;
-
-	// draw a shape
-	game.physics.p2.enableBody(this.player, true);
-	this.player.body.clearShapes();
-	this.player.body.addCircle(this.player.body_size, 0 , 0);
-	this.player.body.data.shapes[0].sensor = true;
 }
 
 //Server will tell us when a new enemy player connects to the server.
 //We create a new enemy in our game.
 function onNewPlayer (data) {
 	//enemy object
-	var new_enemy = new remote_player(data.id, data.x, data.y, data.angle);
-	enemies.push(new_enemy);
-}
-
-//Server tells us there is a new enemy movement. We find the moved enemy
-//and sync the enemy movement with the server
-function onEnemyMove (data) {
-	console.log("moving enemy");
-
-	var movePlayer = findplayerbyid (data.id);
-
-	if (!movePlayer) {
-		return;
-	}
-
-	var newPointer = {
-		x: data.x,
-		y: data.y,
-		worldX: data.x,
-		worldY: data.y,
-	}
-
-	var distance = distanceToPointer(movePlayer.player, newPointer);
-	speed = distance/0.05;
-
-	movePlayer.rotation = movetoPointer(movePlayer.player, speed, newPointer);
-}
-
-//we're receiving the calculated position from the server and changing the player position
-function onInputRecieved (data) {
-
-	//we're forming a new pointer with the new position
-	var newPointer = {
-		x: data.x,
-		y: data.y,
-		worldX: data.x,
-		worldY: data.y,
-	}
-
-	var distance = distanceToPointer(player, newPointer);
-	//we're receiving player position every 50ms. We're interpolating
-	//between the current position and the new position so that player
-	//does jerk.
-	speed = distance/0.05;
-
-	//move to the new position.
-	player.rotation = movetoPointer(player, speed, newPointer);
-
+   console.log("added: "+data.id);
+	enemies.push(data.id);
 }
 
 //This is where we use the socket id.
 //Search through enemies list to find the right enemy of the id.
 function findplayerbyid (id) {
 	for (var i = 0; i < enemies.length; i++) {
-		if (enemies[i].id == id) {
+		if (enemies[i] == id) {
 			return enemies[i];
 		}
 	}
-}
-function getID(data){
 }
 main.prototype = {
 
 	create: function () {
 		game.stage.backgroundColor = 0xE1A193;;
 		console.log("client started");
-		socket.on("connect", onsocketConnected);
-      
-      socket.on("id", function(data){
-      
-   console.log("here");
-   console.log(data.privateId);
-   console.log(data.publicId);
-      });
+      socket.emit("client_started",{});
+      socket.on('connected', onsocketConnected);
 		socket.on('remove_player', onRemovePlayer);
+      socket.on('newPlayer', onNewPlayer)
 		//when the player receives the new input
-		socket.on('input_recieved', onInputRecieved);
 	},
 
 	update: function () {
 		// emit the player input
-
-		//move the player when the player is made
-		if (gameProperties.in_game) {
-
-			//we're making a new mouse pointer and sending this input to
-			//the server.
-			var pointer = game.input.mousePointer;
-
-			//Send a new position data to the server
-			socket.emit('input_fired', {
-				pointer_x: pointer.x,
-				pointer_y: pointer.y,
-				pointer_worldx: pointer.worldX,
-				pointer_worldy: pointer.worldY,
-			});
-		}
 	}
 };
