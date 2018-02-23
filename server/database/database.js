bd841225339e1d8d2464bc40b3239cadd1343209
Max <mbblau423@gmail.com>
2018-02-23 11:37:08 -0800

    Finished early implementation of a database manager and initialized it in main

---
+++ server/database/database.js
@@ -0,0 +1,108 @@
+/**
+ * File to init and manage MongoDb database on the Heroku server
+ * In app.js, call:
+ *    new Database();
+ */
+
+let mongoose = require('mongoose');
+const uri = 'mongodb://glAdmin:GlobalWarming2018@ds243798.mlab.com:43798/heroku_s7k63t29'
+
+class Database{
+   constructor() {
+      mongoose.connect(uri, function(err){
+         if(err) throw err;
+         console.log("Connected!");
+      });
+   
+      this.Player = mongoose.model(
+         'Player',
+         mongoose.Schema({
+            username:   String,
+            password:   String,
+            stats:      {
+               gamesWon:   Number,
+               gamesLost:  Number
+            },
+            friends: [{
+               name: String,
+            }]
+         })
+      );
+   }
+
+   findAll(modelName) {
+      return new Promise((resolve,reject)=>{
+         var model = null;
+         var data = null;
+         if(modelName == 'Player') model = this.Player;
+         else {
+            console.log('Enter valid model name');
+            return;
+         }
+         model.find(function(err, docs) {
+            if(err) reject(err);
+            resolve(docs);
+         });
+      });
+   }
+
+   findOne(modelName, query) {
+      return new Promise((resolve,reject)=>{
+         var model = null;
+         var data = null;
+         if(modelName == 'Player') model = this.Player;
+         else {
+            console.log('Enter valid model name');
+            return
+         }
+         model.find(query, function(err, docs) {
+            if(err) reject(err);
+            resolve(docs);
+         });
+      });
+   }
+
+   remove(modelName, query) {
+      return new Promise((resolve,reject)=>{
+         var model = null;
+         if(modelName == 'Player') model = this.Player;
+         else {
+            console.log('Enter valid model name');
+            return;
+         }
+         model.remove(query, function(err, doc) {
+            if(err) reject(err);
+            console.log('Successfully removed document in collection ' + modelName);
+            resolve(doc);
+         });
+      });
+   }
+
+   insertOne(modelName, doc) {
+      return new Promise((resolve,reject)=>{
+         var model = null;
+         if(modelName == 'Player') model = this.Player;
+         else {
+            console.log('Enter valid model name');
+            return;
+         }
+         var data = new model(doc);
+         data.save(function(err) {
+            if(err) reject(err);
+            console.log("Successfully saved in collection " + modelName + "s");
+         });
+      });
+   }
+
+   insertMany(modelName, documents) {
+      return new Promise((resolve,reject)=>{
+         for(i in documents) {
+            this.insertOne(modelName, documents[i]);
+         }
+      });
+   }
+}
+
+module.exports = {
+   Database: Database
+}
