072f39d27b7f1495449df09592f9aa616386828b
Jacob <jaketerrito@gmail.com>
2018-04-06 15:52:29 -0700

    basic addplayer test case working

--- spec/gameTestSpec.js
+++ spec/gameTestSpec.js
@@ -2,10 +2,9 @@ var gameObject = require("../objects.js");
 var app = require("../app.js");
 
 describe("Test adding a new player", function(){
-   it("returns false because there is no map",function(){
+   it("returns true because there are empty castles",function(){
       game = new gameObject.Game(null,0);
       app.makeMap(game);
-      game.map = new Map();
-      expect(game.addPlayer(0)).toBe(false);
+      expect(game.addPlayer(0)).toBe(true);
    });
 });
