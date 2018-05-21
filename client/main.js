25dd98dba4b382b101370e3088b630dc10363c09
Jacob Territo <jaketerrito@gmail.com>
2018-05-21 01:17:30 -0700

    now works on all devices and there are more sounds

--- client/main.js
+++ client/main.js
@@ -24,9 +24,6 @@ for(unit of units){
 var bannerGFX;
 //var leaveButton;
 
-var march;
-var battle_music;
-
 var main = function(game){
 };
 
@@ -175,7 +172,7 @@ function addNewPlayer(id) {
       }
    }else{
 
-      menu_music.stop();
+      menu_music.pause();
       unit = getUnit();
    }
    let player = new Player(id, unit);
@@ -243,8 +240,8 @@ function endSwipe() {
             swipeNodes.push(node.id);
          }
 	 console.log("emitting: "+ swipeNodes);
-         march.play();
-	 socket.emit('input_fired', {game:gameId, nodes: swipeNodes});
+	 march.play();
+         socket.emit('input_fired', {game:gameId, nodes: swipeNodes});
 	} else {
 	    console.log("swipe failed");
 	}
@@ -399,8 +396,6 @@ main.prototype = {
         game.world.bringToTop(nodeGroup);
 		game.input.onUp.add(endSwipe);
                 
-                battle_music = game.add.audio('battle_music',.7,true);
-                march = game.add.audio('march');
 /*
 		leaveButton = game.add.button(game.camera.x + window.innerWidth, game.camera.y + window.innerHeight, 'button1', function() {
 			if(gameSocket != null) {
