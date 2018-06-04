c663440157e328ef272d2ea8747b4b1c38189742
Jacob Territo <jaketerrito@gmail.com>
2018-06-04 16:22:31 -0700

    unit ready for go

--- spec/battleSpec.js
+++ spec/battleSpec.js
@@ -5,34 +5,42 @@ var player = require("../Player.js");
 
 describe('Test battles', function(){
 
-   it("Checking that battle ends", function(){
-      dummy = new player.Player('ya','ya'); 
-      armya = new armys.Army(dummy,100,null,null,null,null);
-      armyb = new armys.Army(dummy,1,null,null,null,null);
+   it("Checking that battle ends", function(){ 
+      dummya = new player.Player('ya','ya'); 
+      dummyb = new player.Player('na','na'); 
+      armya = new armys.Army(dummya,100,null,null,null,null);
+      armyb = new armys.Army(dummyb,1,null,null,null,null);
 
-      battle = new battles.Battle(armya,dummy,null,armyb,dummy,null,null,null,null);
+      battle = new battles.Battle(null,null,null);
+      battle.addArmy(armya,dummya,null);
+      battle.addArmy(armyb,dummyb,null);
       expect(battle.tick()).toBe(false);
       //battle should end because one army has 100 while the other has 1;
    });
 
    it("Checking that battle does not end off the bat", function(){
-      dummy = new player.Player('ya','ya'); 
-      armya = new armys.Army(dummy,100,null,null,null,null);
-      armyb = new armys.Army(dummy,100,null,null,null,null);
-
-      battle = new battles.Battle(armya,dummy,null,armyb,dummy,null,null,null,null);
+      dummya = new player.Player('ya','ya'); 
+      dummyb = new player.Player('na','na');
+      armya = new armys.Army(dummya,100,null,null,null,null);
+      armyb = new armys.Army(dummyb,100,null,null,null,null);
+      
+      battle = new battles.Battle(null,null,null);
+      battle.addArmy(armya,dummya,null);
+      battle.addArmy(armyb,dummyb,null);
       expect(battle.tick()).toBe(true);
       //Shouldn't end because these are large armies, take multiple ticks to fight
    });
 
-   it("Checking that ties break the game", function(){
-      dummy = new player.Player('ya','ya'); 
-      armya = new armys.Army(dummy,1,null,null,null,null);
-      armyb = new armys.Army(dummy,1,null,null,null,null);
-
-      battle = new battles.Battle(armya,dummy,null,armyb,dummy,null,null,null,null);
-      expect(battle.tick()).toBe(true);
-      //Should end as tie because it is a 1v1, this is broken
+   it("Checking that armies merge", function(){
+      dummya = new player.Player('ya','ya'); 
+      armya = new armys.Army(dummya,1,null,null,null,null);
+      armyb = new armys.Army(dummya,1,null,null,null,null);
+      
+      battle = new battles.Battle(null,null,null);
+      battle.addArmy(armya,dummya,null);
+      battle.addArmy(armyb,dummya,null);
+      expect(battle.tick()).toBe(false);
+      //Should end because there are no enemies
    });
 });
 
