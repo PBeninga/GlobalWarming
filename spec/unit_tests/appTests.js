9753288b644ed748fc4b682b75007179e8020841
adambeymer <adambeymer@gmail.com>
2018-06-04 16:44:27 -0700

    added a unit testing folder

---
+++ spec/unit_tests/appTests.js
@@ -0,0 +1,7 @@
+ describe("Test increment all troops:",function(){
+
+    it("test that all troops are incremented the same way",function(){ 
+        console.log("test running???")
+    });
+
+ });
