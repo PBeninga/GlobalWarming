1fcfa4960263bc65487249e3dc35c30ed179c18d
LoumenaMax <loumenamax@gmail.com>
2018-06-06 14:26:58 -0700

    Added more tests

--- spec/unit_tests/movingArmySpec.js
+++ spec/unit_tests/movingArmySpec.js
@@ -7,7 +7,7 @@ var playerObject = require("../../Player.js");
 
 describe('Test moving armies', function(){
 
-   console.log("Running battle unit Tests");
+   console.log("Running moving army unit Tests");
 
    it("Checking that moving army initializes properly", function(){
 		var dummy = new playerObject.Player('ya','ya');
@@ -20,13 +20,44 @@ describe('Test moving armies', function(){
 		swipePath.push({x:nodeb.x, y:nodeb.y});
 
 		var movingArmy = new movingArmyObject.MovingArmy(army, swipePath, null);
-
 		expect(movingArmy.angle).toEqual(Math.atan2(0, 100));
-		expect(movingArmy.distance).toEqual(100);
+		expect(movingArmy.distance).toEqual(48);
 		expect(movingArmy.moveX).toEqual(army.movementSpeed * Math.cos(movingArmy.angle));
 		expect(movingArmy.moveY).toEqual(army.movementSpeed * Math.sin(movingArmy.angle));
-		expect(movingArmy.percent).toEqual(100);
+		expect(movingArmy.percent).toEqual(0);
 		expect(movingArmy.movePercent).toEqual((army.movementSpeed / movingArmy.distance) * 100);
       //battle should end because one army has 100 while the other has 1;
    });
+
+	it("Checking the moving army moveUp function ends properly", function(){
+		var dummy = new playerObject.Player('ya','ya');
+		var nodea = new mapObject.MapNode(0, 0, [1], 0);
+		var nodeb = new mapObject.MapNode(100, 0, [0], 1);
+		var army = new armyObject.Army(dummy,100,nodea,0,0,null);
+
+		var swipePath = [];
+		swipePath.push({x:nodea.x, y:nodea.y});
+		swipePath.push({x:nodeb.x, y:nodeb.y});
+
+		var movingArmy = new movingArmyObject.MovingArmy(army, swipePath, null);
+		expect(movingArmy.moveUp()).toBe(false);
+		//battle should end because one army has 100 while the other has 1;
+	});
+
+	it("Checking the moving army moveUp function continues properly", function(){
+		var dummy = new playerObject.Player('ya','ya');
+		var nodea = new mapObject.MapNode(0, 0, [1], 0);
+		var nodeb = new mapObject.MapNode(100, 0, [0, 2], 1);
+		var nodec = new mapObject.MapNode(100, 100, [1], 2);
+		var army = new armyObject.Army(dummy,100,nodea,0,0,null);
+
+		var swipePath = [];
+		swipePath.push({x:nodea.x, y:nodea.y});
+		swipePath.push({x:nodeb.x, y:nodeb.y});
+		swipePath.push({x:nodec.x, y:nodec.y});
+
+		var movingArmy = new movingArmyObject.MovingArmy(army, swipePath, null);
+		expect(movingArmy.moveUp()).toBe(true);
+		//battle should end because one army has 100 while the other has 1;
+	});
 });
