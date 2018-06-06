c8cbf02793d95050330b3ca2678e3e6717fcc5e3
adambeymer <adambeymer@gmail.com>
2018-06-06 12:12:33 -0700

    tests

--- spec/unit_tests/appTests.js
+++ spec/unit_tests/appTests.js
@@ -6,7 +6,7 @@ describe('unit tests', function(){
 
    it("Testing app and game", function(){
       
-      // TEST 1 ensure that games are added correctly
+      // TEST 1 app.js for adding games
       expect( web_app.games.size == 0 ).toBe(true);
       var game1 = web_app.findGame(000);
       expect( web_app.games.size == 1 ).toBe(true);
@@ -14,12 +14,27 @@ describe('unit tests', function(){
       expect( web_app.games.size == 1 ).toBe(true);
       expect( game1 == game2 ).toBe(true);
       
-      // TEST 2 ensure that 
+      // TEST 2 Player.js for checking active player
+      expect( game1.playerPool.containsActive(000) ).toBe(true);
 
 
-      // TEST 2 ensure that games are removed correctly
-      game1.removePlayer(000);
+      // TEST 3 Game.js  Ensure the correct State
+      expect( game1.gameState == 0  ).toBe(true);
       
+      
+      // TEST 4 Game.js Ensure we can start the game
+      game1.tick();
+      expect( game1.gameState == 1  ).toBe(true);
+      
+      
+      // TEST 5  Game.js for removing player
+      game1.removePlayer(000);
+      expect( game1.playerPool.containsActive(000) ).toBe(false);
+      
+      
+      // TEST 6 app.js removing a game
+      game1.removeGame(game1.roomid);
+      expect( web_app.games.size == 0 ).toBe(true); 
       
    });
 
