32b1c9faf290ced8fe2f4d1acf869d7a857c7c29
adambeymer <adambeymer@gmail.com>
2018-06-06 14:01:33 -0700

    testing some stuff

--- spec/unit_tests/appTests.js
+++ spec/unit_tests/appTests.js
@@ -1,12 +1,12 @@
-var web_app = require("../../app.js");
-var game = require("../../Game.js");
-var playerObject = require('../../Player.js');
 
-describe('unit tests', function(){
+describe('Adding Games test', function(){
 
-   it("Testing app and game", function(){
+   
+   it("Add games", function(){
       
-      // TEST 1 app.js for adding games
+      var web_app = require("../../app.js");
+      var game = require("../../Game.js");
+      var playerObject = require('../../Player.js');
       expect( web_app.games.size == 0 ).toBe(true);
       var game1 = web_app.findGame(000);
       expect( web_app.games.size == 1 ).toBe(true);
@@ -14,12 +14,40 @@ describe('unit tests', function(){
       expect( web_app.games.size == 1 ).toBe(true);
       expect( game1 == game2 ).toBe(true);
       
-      // TEST 2 Player.js for checking active player
-      expect( game1.playerPool.containsActive(000) ).toBe(true);
+   });
+   
+   for(var i = 0; i<100000; i++ ){} // Wait for Add games to finish
 
+ });
+
+/*
+describe('Check for active Player', function(){
+
+   it("Check for active player", function(){
+      
+      var web_app = require("../../app.js");
+      var game = require("../../Game.js");
+      var playerObject = require('../../Player.js');
+   
+      var game = web_app.findGame(002); // Should return game 1
+      expect( game.playerPool.containsActive(000) ).toBe(true);
+      
+   });
+   
+ //  for(var i = 0; i<10000; i++ ){} // Wait for Add games to finish
+
+ });
+
+
+*/
+
+
+
+
+
+/*
 
       // TEST 3 Game.js  Ensure the correct State
-      expect( game1.gameState == 0  ).toBe(true);
       
       
       // TEST 4 Game.js Ensure we can start the game
@@ -35,7 +63,4 @@ describe('unit tests', function(){
       // TEST 6 app.js removing a game
       game1.removeGame(game1.roomid);
       expect( web_app.games.size == 0 ).toBe(true); 
-      
-   });
-
- });
+      */
